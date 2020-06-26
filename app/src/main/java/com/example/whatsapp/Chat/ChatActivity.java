9e9095e3757b6fb71a55ac314e4e3d09c42a0150
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2020-06-26 23:15:15 +0530

    Images getting uploaded on the friebase

--- app/src/main/java/com/example/whatsapp/Chat/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/Chat/ChatActivity.java
@@ -7,19 +7,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.whatsapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,9 +35,9 @@ import java.util.Map;
 public class ChatActivity extends AppCompatActivity {
 
 
-    private RecyclerView mChat;
-    private RecyclerView.Adapter mChatAdapter;
-    private RecyclerView.LayoutManager mChatLayoutManager;
+    private RecyclerView mChat,mMedia;
+    private RecyclerView.Adapter mChatAdapter,mMediaAdapter;
+    private RecyclerView.LayoutManager mChatLayoutManager,mMediaLayoutManager;
 
     ArrayList<MessageObject> messageList;
     String chatID;
@@ -43,15 +50,26 @@ public class ChatActivity extends AppCompatActivity {
         mChatDb= FirebaseDatabase.getInstance().getReference().child("chat").child(chatID);
 
         Button mSend=findViewById(R.id.send);
+        Button mAddMedia=findViewById(R.id.addMedia);
         mSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendMessage();
             }
         });
-        initializeRecyclerView();
+        mAddMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+
+            }
+        });
+        initializeMessage();
+        initializeMedia();
         getChatMessages();
     }
+
+
 
     private void getChatMessages() {
         mChatDb.addChildEventListener(new ChildEventListener() {
@@ -83,22 +101,64 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
+    int totalMediaUploaded=0;
+    ArrayList<String> medialList=new ArrayList<>();
+    EditText mMessage;
+
     private void sendMessage() {
-         EditText mMessage=findViewById(R.id.mes);
-        if(!mMessage.getText().toString().isEmpty()){
-            DatabaseReference newMessageDb= FirebaseDatabase.getInstance().getReference().child("chat").child(chatID).push();
+        mMessage=findViewById(R.id.mes);
 
-           Map newMessageMap=new HashMap<>();
-           newMessageMap.put("text",mMessage.getText().toString());
-           newMessageMap.put("creator",FirebaseAuth.getInstance().getUid());
+            String messageId =mChatDb.push().getKey();
+            final DatabaseReference newMessageDb= mChatDb.child(messageId);
+            final Map newMessageMap=new HashMap<>();
+            newMessageMap.put("creator",FirebaseAuth.getInstance().getUid());
 
-            newMessageDb.updateChildren(newMessageMap);
-        }
+            if(!mMessage.getText().toString().isEmpty())
+                newMessageMap.put("text",mMessage.getText().toString());
+
+
+            if(!mediaUriList.isEmpty()){
+                for(String medialUri: mediaUriList){
+                    String mediaId = newMessageDb.child("media").push().getKey();
+                    medialList.add(mediaId);
+                    final StorageReference filePath=FirebaseStorage.getInstance().getReference().child("chat").child(chatID).child(messageId).child(mediaId);
+                    UploadTask uploadTask=filePath.putFile(Uri.parse(medialUri));
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    newMessageMap.put("/media/" + medialList.get(totalMediaUploaded) + "/",uri.toString());
+                                    totalMediaUploaded++;
+                                    if(totalMediaUploaded==mediaUriList.size()){
+                                        updateDatabaseWithNewMessage(newMessageDb,newMessageMap);
+                                    }
+                                }
+                            });
+                        }
+                    });
+
+                }
+            }else{
+                if(!mMessage.getText().toString().isEmpty())
+                    updateDatabaseWithNewMessage(newMessageDb,newMessageMap);
+            }
+
+
+
+    }
+
+    private  void updateDatabaseWithNewMessage(DatabaseReference newMessageDb,Map newMessageMap){
+        newMessageDb.updateChildren(newMessageMap);
         mMessage.setText(null);
+        mediaUriList.clear();
+        medialList.clear();
+        mMediaAdapter.notifyDataSetChanged();
     }
 
     @SuppressLint({"WrongViewCast", "WrongConstant"})
-    private void initializeRecyclerView(){
+    private void initializeMessage(){
 
         messageList = new ArrayList<MessageObject>();
         mChat= findViewById(R.id.messageList);
@@ -108,5 +168,50 @@ public class ChatActivity extends AppCompatActivity {
         mChat.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new MessageAdapter(messageList);
         mChat.setAdapter(mChatAdapter);
+    }
+
+
+    int PICK_IMAGE_INTENT=1;
+    ArrayList<String> mediaUriList=new ArrayList<>();
+
+    @SuppressLint("WrongConstant")
+    private void initializeMedia(){
+
+        mediaUriList = new ArrayList<>();
+        mMedia= findViewById(R.id.mediaList);
+        mMedia.setNestedScrollingEnabled(false);
+        mMedia.setHasFixedSize(false);
+        mMediaLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.HORIZONTAL, false);
+        mMedia.setLayoutManager(mMediaLayoutManager);
+        mMediaAdapter = new MediaAdapter(getApplicationContext(),mediaUriList);
+        mMedia.setAdapter(mMediaAdapter);
+    }
+
+
+    private void openGallery() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture(s)"),PICK_IMAGE_INTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode== RESULT_OK){
+            if(requestCode==PICK_IMAGE_INTENT) {
+                if (data.getClipData() == null) {
+                    mediaUriList.add(data.getData().toString());
+                }else{
+                    for(int i=0;i<data.getClipData().getItemCount();i++){
+                        mediaUriList.add(data.getClipData().getItemAt(i).getUri().toString());
+                    }
+                }
+
+                mMediaAdapter.notifyDataSetChanged();
+
+            }
+        }
     }
 }

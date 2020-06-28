eeda8ef97e57a45fd68a0e8ad7919f1116e30219
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2020-06-28 18:23:01 +0530

    UI/UX done for Chat Activity (very bsaic no left right alignment)

--- app/src/main/java/com/example/whatsapp/Chat/MessageAdapter.java
+++ app/src/main/java/com/example/whatsapp/Chat/MessageAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -70,7 +71,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
   class MessageViewHolder extends RecyclerView.ViewHolder{
 
         TextView mMessage,mSender;
-        Button mViewMedia;
+        ImageButton mViewMedia;
 
         public LinearLayout mLayout;
        MessageViewHolder(View view){

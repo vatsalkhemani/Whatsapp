2ee1ea7d2799edebe5fea61804851da0b22d7f93
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2020-06-28 18:01:27 +0530

    UI/UX done for Chat List

--- app/src/main/java/com/example/whatsapp/ProfileActivity.java
+++ app/src/main/java/com/example/whatsapp/ProfileActivity.java
@@ -14,6 +14,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.example.whatsapp.Chat.ChatListAdapter;
@@ -36,7 +37,7 @@ public class ProfileActivity extends AppCompatActivity {
     private RecyclerView mChatList;
     private RecyclerView.Adapter mChatListAdapter;
     private RecyclerView.LayoutManager mChatListLayoutManager;
-    Button mGroup;
+    ImageButton mGroup;
 
     ArrayList<ChatObject> chatList;
 
@@ -60,8 +61,8 @@ public class ProfileActivity extends AppCompatActivity {
 
 
 
-        Button mLogout = findViewById(R.id.logout);
-        Button mFindUser = findViewById(R.id.findUser);
+        ImageButton mLogout = findViewById(R.id.logout);
+        ImageButton mFindUser = findViewById(R.id.findUser);
         mGroup=findViewById(R.id.group);
 
         mGroup.setOnClickListener(new View.OnClickListener() {
